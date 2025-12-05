6d4eaca23743b87a419d0921562799139752ff41
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-12-05 15:22:56 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { motion } from 'motion-v'
+</script>
+
+<template>
+  <motion.div class="box" :animate="{ rotate: 360 }" :transition="{ duration: 1 }" />
+</template>
+
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: #0cdcf7;
+  border-radius: 5px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Component from 'unrplugin-vue-components/vite'
+import MotionResolver from 'motion-v/resolver'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss(), Component(), MotionResolver()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
